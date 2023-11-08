2050c165539dcdb8800840e830ee3d3196865c4a
Victor Hugo <abjurandam@gmail.com>
2023-11-08 15:26:50 +0000

    Inicio etapa3
    Faremos parsing da expressao

---
+++ etapa3.cpp
@@ -0,0 +1,23 @@
+#include "minhasfuncoes.h"
+#include "posfixa.h"
+#include "tad_pilha_estatica.h"
+#include <stdio.h>
+
+int main(void) {
+
+  char expressao[max];
+  scanf("%s", expressao);
+
+  T_Pilha pilhaExpressao;
+  iniciarPilha(&pilhaExpressao);
+  gerar_posfixa(&pilhaExpressao, expressao);
+  listar_pilha(&pilhaExpressao);
+
+  T_Pilha pilhaResultado;
+  iniciarPilha(&pilhaResultado);
+  calcular_resultado(&pilhaExpressao, &pilhaResultado);
+
+  printf("%d ", pilhaResultado.dados[0].campo);
+
+  return 0;
+}
--- posfixa.h
+++ posfixa.h
@@ -7,26 +7,35 @@
 enum Operacoes { MULT = '*', SOMA = '+', SUB = '-', DIV = '/' };
 
 void gerar_posfixa(T_Pilha *plOutput, char exp[]) {
+  // contador?
 
   T_Pilha plOperadores;
   iniciarPilha(&plOperadores);
 
-  T_Item item;
+  T_Item itemOperador;
+  T_Item itemNumero;
+  T_Item itemPercorrendo;
+
+  int contNumero = 0;
 
   for (int i = 0; i < strlen(exp); i++) {
 
-    item.campo = exp[i];
+    itemPercorrendo.campo = exp[i];
 
     switch (exp[i]) {
 
+      // CONTINUAR AQUI ********** mexi zoado
     case '0' ... '9':
-      inserir(plOutput, item);
+      if (contNumero == 0)
+        inserir(plOutput, item);
       break;
 
+      // se chegarmos aqui, empilhar o numero concatenado
     case MULT ... DIV:
       inserir(&plOperadores, item);
       break;
 
+      // se chegarmos aqui, empilhar o numero concatenado
     case ')': {
       inserir(plOutput, plOperadores.dados[plOperadores.topo - 1]);
       remover(&plOperadores);
