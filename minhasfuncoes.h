d2aa0e012ae64702d153fab4afa8aaefe6be1d58
Victor Hugo <abjurandam@gmail.com>
2023-11-11 13:09:10 +0000

    Add files via upload

--- minhasfuncoes.h
+++ minhasfuncoes.h
@@ -10,7 +10,13 @@ void listar_pilha(T_Pilha *pilhaExpressao) {
 
   for (int i = 0; i < pilhaExpressao->qtdeAtual; i++) {
 
-    printf("%c ", pilhaExpressao->dados[i].campo);
+    if (pilhaExpressao->dados[i].flagZeroIntUmChar == 0) {
+
+      printf("%d ", pilhaExpressao->dados[i].campo);
+    } else {
+
+      printf("%c ", pilhaExpressao->dados[i].campo);
+    }
   }
   printf("\n");
 }
@@ -43,17 +49,16 @@ void calcular_resultado(T_Pilha *pilhaExpressao, T_Pilha *pilhaResultado) {
   for (int i = 0; i < pilhaExpressao->qtdeAtual; i++) {
     T_Item *aSerTestado = &pilhaExpressao->dados[i];
 
-    if (isdigit(aSerTestado->campo)) {
+    if (aSerTestado->flagZeroIntUmChar == 0) {
 
-      // operacao ASCII
-      int value = aSerTestado->campo - '0';
+      int value = aSerTestado->campo;
 
       T_Item item;
       item.campo = value;
       inserir(pilhaResultado, item);
     }
 
-    else if (strchr("+-*/", aSerTestado->campo)) {
+    else {
 
       int operador1 = meuDesempilhar(pilhaResultado);
       int operador2 = meuDesempilhar(pilhaResultado);
@@ -86,4 +91,61 @@ void calcular_resultado(T_Pilha *pilhaExpressao, T_Pilha *pilhaResultado) {
   }
 }
 
+void meuGerarPosfixa(T_Pilha *plOutput, char exp[]) {
+  T_Pilha plOperadores;
+  iniciarPilha(&plOperadores);
+
+  T_Item item;
+
+  int i = 0;
+  while (exp[i] != '\0') {
+    if (isdigit(exp[i])) {
+
+      int num = 0;
+      while (isdigit(exp[i])) {
+        num = num * 10 + (exp[i] - '0');
+        i++;
+      }
+
+      item.campo = num;
+      item.flagZeroIntUmChar = 0;
+      inserir(plOutput, item);
+    } else if (strchr("+-*/", exp[i]) != NULL) {
+
+      item.campo = exp[i];
+      item.flagZeroIntUmChar = 1;
+
+      while (!chkPilhaVazia(&plOperadores) &&
+             plOperadores.dados[plOperadores.topo - 1].campo != '(') {
+        inserir(plOutput, plOperadores.dados[plOperadores.topo - 1]);
+        remover(&plOperadores);
+      }
+
+      inserir(&plOperadores, item);
+      i++;
+    } else if (exp[i] == '(') {
+      item.campo = '(';
+      item.flagZeroIntUmChar = 1;
+      inserir(&plOperadores, item);
+      i++;
+    } else if (exp[i] == ')') {
+
+      while (!chkPilhaVazia(&plOperadores) &&
+             plOperadores.dados[plOperadores.topo - 1].campo != '(') {
+        inserir(plOutput, plOperadores.dados[plOperadores.topo - 1]);
+        remover(&plOperadores);
+      }
+
+      remover(&plOperadores);
+      i++;
+    } else {
+      i++;
+    }
+  }
+
+  while (!chkPilhaVazia(&plOperadores)) {
+    inserir(plOutput, plOperadores.dados[plOperadores.topo - 1]);
+    remover(&plOperadores);
+  }
+}
 #endif
